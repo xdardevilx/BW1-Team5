4a25f9ead4192921e00a7907aac40f2374a30b61
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2023-12-13 15:50:12 +0100

    mod time

--- js/time.js
+++ js/time.js
@@ -1,15 +1,21 @@
-let countdownDate = new Date().getTime() + 31000;
+let intervalloTimer = null;
+let tempoRimanente = 30;
 const startTimer = function () {
-  const time = setInterval(function () {
+  clearInterval(intervalloTimer);
+  let countdownDate = new Date().getTime() + tempoRimanente + 30000;
+  /* const barra = document.getElementById("progress"); */
+  intervalloTimer = setInterval(function () {
     const now = new Date().getTime();
     const distance = countdownDate - now;
-    const seconds = Math.floor(distance / 1000);
-    document.getElementById("seconds").innerHTML = seconds;
+
+    let seconds = Math.floor(distance / 1000);
+    console.log(seconds);
+    document.getElementById("seconds").innerText = seconds;
     if (distance < 0) {
-      clearInterval(time);
-      document.getElementById("seconds").innerHTML = mostraProssimaDomanda();
+      clearInterval(intervalloTimer);
+      document.getElementById("seconds").innerText = mostraProssimaDomanda();
       document.getElementById("seconds").innerText = "30";
-      countdownDate = new Date().getTime() + 31000;
+      countdownDate = new Date().getTime() + 30000;
       startTimer();
     }
   }, 1000);
