c84b2053e4764665e84943c764ffac53d59b677c
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2023-12-13 14:04:36 +0100

    mod

--- js/result.js
+++ js/result.js
@@ -1,20 +1,35 @@
-const risposteCorrette = [1, 1, 1, 1, 1];
-const risposteErrate = [1, 1, 1, 1, 1];
+console.log(window.location.href);
+const risposteCorrette = [1, 1, 1, 1];
+const risposteErrate = [1, 1, 1, 1, 1, 1];
+const numeroRisposte = function () {
+  const correctP = document.getElementById("correctQuestions");
+  const wrongP = document.getElementById("wrongQuestions");
+  const correctLength = risposteCorrette.length + "/10 questions";
+  const wrongLength = risposteErrate.length + "/10 questions";
+
+  correctP.innerText = correctLength;
+  wrongP.innerText = wrongLength;
+};
+
+numeroRisposte();
 
 const risultatoTest = function () {
-  const h3Circle1 = document.getElementById("h3Cirle1");
+  const risposteCorrette = [1, 1, 1, 1];
+  const risposteErrate = [1, 1, 1, 1, 1, 1];
+
+  const h3Circle1 = document.getElementById("h3Circle1");
   const h3Circle2 = document.getElementById("h3Circle2");
   const pCircle = document.getElementById("pCircle");
 
-  if (risposteCorrette > 5) {
-    h3Circle1.innerText("Congratulations!");
-    h3Circle2.innerText("You passed the exam.");
-    pCircle.innerText(
-      "We'll send you the certificate in few minutes.Ceck your email (including promotions / spam folder)"
-    );
-    h3Circle1.innerText("I'm sorry");
-    h3Circle2.innerText("You not passed the exam.");
-    pCircle.innerText("You must repete the exam.");
+  if (risposteCorrette.length >= 5) {
+    h3Circle1.innerText = "Congratulations!";
+    h3Circle2.innerText = "You passed the exam.";
+    pCircle.innerText =
+      "We'll send you the certificate in few minutes.Ceck your email (including promotions / spam folder)";
+  } else {
+    h3Circle1.innerText = "I'm sorry";
+    h3Circle2.innerText = "You not passed the exam.";
+    pCircle.innerText = "You must repete the exam.";
   }
 };
 
@@ -25,32 +40,33 @@ const conteggioRisposte = function () {
   const risposteErratePercentuale = risposteErrate.length * 10;
   const percentualeCorrette = document.getElementById("correct");
   const percentualeErrate = document.getElementById("wrong");
-  percentualeCorrette.innerText = `${risposteEsattePercentuale} %`;
-  percentualeErrate.innerText = `${risposteErratePercentuale} %`;
+  percentualeCorrette.innerText = `${risposteErratePercentuale} %`;
+  percentualeErrate.innerText = `${risposteEsattePercentuale} %`;
   const canvas = document.getElementById("graficoCiambella");
 
-  let etichette = ["corrette", "errate"];
+  let etichette = ["errate", "corrette"];
   let data = [risposteErratePercentuale / 10, risposteEsattePercentuale / 10];
-  let option = {};
 
-  let color = ["#900080", "#00FFFF"];
-  let ciambella = new Chart(canvas, {
+  // Chart.default.global.defaultradius = "20%";
+  let color = ["#D20094", "#00FFFF"];
+  const ciambella = new Chart(canvas, {
     type: "doughnut",
-
     data: {
       labels: etichette,
 
       datasets: [
         {
           backgroundColor: color,
-          radius: "90%",
           data: data,
         },
       ],
     },
-
     options: {
-      cutoutPercentage: 50, // Imposta la percentuale di spessore tagliato
+      cutoutPercentage: 70,
+      radius: "90%",
+      legend: {
+        display: false,
+      },
     },
   });
   return ciambella;
