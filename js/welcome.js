73968ce50a197183fa5a1f06e997b00d2c0f4e6c
Fejz7 <ZOAC@outlook.it>
2023-12-15 11:55:05 +0100

    aggiunta selezione difficoltà

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,25 @@
-const formReference = document.getElementsByTagName('form')[0]
+const select = document.getElementById('dif')
+
+const controlloDifficoltà = function() {
+  if (select.value === 'hard') {
+    let numeroDomande = 10
+    return numeroDomande
+  }
+  else if (select.value === 'medium') {
+    let numeroDomande = 7
+    return numeroDomande
+  }
+  else   {
+    let numeroDomande = 5
+    return numeroDomande
+  }
+}
+
+const formReference = document.getElementsByTagName('form')[1]
 formReference.addEventListener('submit', function (e) {
   e.preventDefault()
-  window.location.href = './benchmark.html'
+  console.log(select.value)
+  controlloDifficoltà()
+  window.location.href = './benchmark.html'+'?domande ='+ controlloDifficoltà()
 })
 
